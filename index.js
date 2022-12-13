c0553bde54c7ad6dd85085f63931e01cf8644336
mikelaslam <kipkorir385@gmail.com>
2022-12-13 03:59:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -20,14 +20,25 @@ function isPalindrome(char) {
   }
 }
   // Write your algorithm here
+/* 1. Initialize a function called tracebackString(char)
+   2. Define a variable charArray = charsplit("")
+   3. Reverse the array
+   4. Initialize the function Palindrome which receives a string as an argument
+   5. Use the if statements to achieve the aim
 
 
  /* 
-   Add your pseudocode here
- */
+   //Add your pseudocode here
+   declare a function called tracedbackString
+   Initialize an array tracedbackArray = charArray.reverse();
+   iterate over each item in the array
+   if the character is tracedback return result, if it isn't teturn otherwise.
+ */ 
 
- /*
-  Add written explanation of your solution here
+
+  /*Add written explanation of your solution here
+  After Iterating over each item in the array, if the item is a palindrome it returns true,
+  if it is not a palindrome it returns false.
 */
 
 // You can run `node index.js` to view these console logs
